8503ce67f62311c116c4774f4ca2ed7c83e2b6de
rbckman <rbckman@localhost.localdomain>
2016-11-02 23:11:57 +0200

    oldpirates ye de rabbai

--- tarina.py
+++ tarina.py
@@ -2,19 +2,6 @@
 # -*- coding: utf-8 -*-
 
 #Tarina - The DIY camera for filmmakers, vloggers, travellers & hackers.
-#Copyright (C) 2016  Robin Bäckman
-
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, version 2
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import picamera
 import os
@@ -1452,3 +1439,16 @@ if __name__ == '__main__':
         main()
     except:
         print 'Unexpected error : ', sys.exc_info()[0], sys.exc_info()[1]
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 2
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
