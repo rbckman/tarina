c1634856b1786ec0bfe001fc7b70e53c5c34d390
rbckman <robinbackman@gmail.com>
2018-08-10 16:06:18 +0300

    added tarina server

---
+++ srv/tarinaserver.py
@@ -0,0 +1,26 @@
+import web
+import os
+
+films = []
+
+urls = (
+    '/', 'index'
+)
+
+render = web.template.render('templates/')
+
+class index:
+    def GET(self):
+        films = os.walk('static/Videos/').next()[1]
+        renderedfilms = []
+        unrenderedfilms = []
+        for i in films:
+            if os.path.isfile('static/Videos/' + i + '/' + i + '.mp4') == True:
+                renderedfilms.append(i)
+            else:
+                unrenderedfilms.append(i)
+        return render.index(renderedfilms, unrenderedfilms)
+
+if __name__== "__main__":
+    app = web.application(urls, globals())
+    app.run()
