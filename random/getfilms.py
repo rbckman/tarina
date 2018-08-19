58f3e96ec8eeeb0fb556b28776884bb86a849bd7
rbckman <rob@robinsfirma.com>
2018-08-19 10:36:46 +0300

    use tuple list instead of a dict

--- random/getfilms.py
+++ random/getfilms.py
@@ -7,15 +7,13 @@ rundir = os.path.dirname(__file__)
 if rundir != '':
     os.chdir(rundir)
 
-filmlibrary = {}
-filmfolder = '/home/pi/Videos/'
+films_sorted = []
+filmfolder = '/home/nemo/'
 
 films = os.walk(filmfolder).next()[1]
-renderedfilms = []
-unrenderedfilms = []
 for i in films:
     if os.path.isfile(filmfolder + i + '/' + 'settings.p') == True:
         lastupdate = os.path.getmtime(filmfolder + i + '/' + 'settings.p')
-        filmlibrary[i]=lastupdate
-
-print filmlibrary
+        append.films_sorted(i,lastupdate)
+films_sorted = sorted(films, key=lambda tup: tup[1])
+print films_sorted
