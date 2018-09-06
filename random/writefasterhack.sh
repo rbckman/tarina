9575260f2d44f6865fd23c97c7c16f9d8be1c5ca
rbckman <rbckman@rbckman>
2018-09-06 19:55:02 +0300

    unused things removed

--- random/writefasterhack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 3 >/proc/sys/vm/dirty_background_ratio
-echo 50 >/proc/sys/vm/dirty_ratio
-echo 300 >/proc/sys/vm/dirty_writeback_centisecs
-echo 300 >/proc/sys/vm/dirty_expire_centisecs
-
