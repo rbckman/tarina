65bfdaa717dd904cc1ae82f97aff0f14a9421fd2
rbckman <rbckman@localhost.localdomain>
2019-04-19 13:25:49 +0300

    timelapse duration should be able to go to 0.1

--- tarina.py
+++ tarina.py
@@ -1,20 +1,8 @@
 #/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#Tarina - a video & audio recorder with glue
-#Copyright 2016 - 2019 Robin Bäckman
-
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
-
-#http://www.apache.org/licenses/LICENSE-2.0
-
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Tarina - a DIY Raspberry Pi Video & Audio Recorder with Glue
+# by Robin Bäckman
 
 import picamera
 import numpy as np
@@ -479,12 +467,12 @@ def timelapse(beeps,camera,foldername,filename,tarinafolder):
         if pressed == 'up' and menu[selected] == 'BETWEEN:':
             between = between + 0.1
         elif pressed == 'down' and menu[selected] == 'BETWEEN:':
-            if between > 0.2:
+            if between > 0.1:
                 between = between - 0.1
         elif pressed == 'up' and menu[selected] == 'DURATION:':
             duration = duration + 0.1
         elif pressed == 'down' and menu[selected] == 'DURATION:':
-            if duration > 0.2:
+            if duration > 0.1:
                 duration = duration - 0.1
         elif pressed == 'up' or pressed == 'down' and menu[selected] == 'SOUND:':
             if sound == False:
