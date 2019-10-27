16923c04c3ad5c075a72fe463425a1e926e9af97
rob <rob@tarina.org>
2019-10-27 22:20:12 +0200

    wifi off by default

--- tarina.py
+++ tarina.py
@@ -1918,8 +1918,8 @@ def main():
     #TURN OFF WIFI AND TARINA SERVER
     serverstate = 'off'
     wifistate = 'off'
-    #run_command('sudo iwconfig wlan0 txpower off')
-    #serverstate = tarinaserver(False)
+    run_command('sudo iwconfig wlan0 txpower off')
+    serverstate = tarinaserver(False)
 
     foldername = filmfolder + filmname + '/' + 'scene' + str(scene).zfill(3) +'/shot' + str(shot).zfill(3) + '/'
     filename = 'take' + str(take).zfill(3)
