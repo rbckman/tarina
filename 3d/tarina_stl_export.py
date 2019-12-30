78298ce09f88bfc9df10c53e8b21c476c39fb044
Robin <rob@tarina.org>
2019-12-30 15:05:11 +0200

    clean up blender file

--- 3d/tarina_stl_export.py
+++ 3d/tarina_stl_export.py
@@ -11,91 +11,91 @@ if not basedir:
 
 scene = bpy.context.scene
 
-obj_active = scene.objects.active
+##obj_active = scene.objects.active
 selection = bpy.context.selected_objects
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Body']
-o.select = True
+o.select_set(True)
 o = bpy.data.objects['Body Support']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-body.stl", check_existing=True, axis_forward='-Z', axis_up='-Y', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Left-Side']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-left-side.stl", check_existing=True, axis_forward='-Y', axis_up='X', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Right-Side']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-right-side.stl", check_existing=True, axis_forward='-Y', axis_up='-X', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Hdmi-Cap']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-hdmi-cap.stl", check_existing=True, axis_forward='X', axis_up='Z', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Button-Plate-Bottom']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-button-plate-bottom.stl", check_existing=True, axis_forward='X', axis_up='Z', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Button-Plate-Upper']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-button-plate-upper.stl", check_existing=True, axis_forward='-X', axis_up='-Z', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Screen-Lid']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-screen-lid.stl", check_existing=True, axis_forward='Z', axis_up='Y', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Picamera-Bridge']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-picamera-bridge.stl", check_existing=True, axis_forward='Y', axis_up='X', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Picamera-Body']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-picamera-body.stl", check_existing=True, axis_forward='Z', axis_up='Y', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Picamera-Body-Lid']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-picamera-body-lid.stl", check_existing=True, axis_forward='-Z', axis_up='-Y', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Mic-Body']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-mic-body.stl", check_existing=True, axis_forward='-Z', axis_up='-Y', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
 
 bpy.ops.object.select_all(action='DESELECT')
 o = bpy.data.objects['Mic-Lid']
-o.select = True
-bpy.context.scene.objects.active = o
+o.select_set(True)
+bpy.context.view_layer.objects.active = o
 bpy.ops.export_mesh.stl(filepath=basedir + "tarina-mic-lid.stl", check_existing=True, axis_forward='Z', axis_up='Y', filter_glob="*.stl", use_selection=True, global_scale=1.0, use_scene_unit=False, ascii=False, use_mesh_modifiers=True, batch_mode='OFF')
 bpy.ops.object.select_all(action='DESELECT')
